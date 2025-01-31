f38fa4d3d64250c0102dfee63b39ee5a2da3f27a
Franco_SJ <jean.jara.fs@gmail.com>
2025-01-31 03:09:37 -0300

    feat: elimine comentarios innecesarios y estilos que ya no se ocupaban

--- frontend/src/app/pages/chat-page/chat-list/chat-list.component.ts
+++ frontend/src/app/pages/chat-page/chat-list/chat-list.component.ts
@@ -1,11 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
-import { MatDialog } from '@angular/material/dialog';
 import { SearchDialogComponent } from '../search-dialog/search-dialog.component';
 import { UserSharingService } from '../../../services/user-sharing.service';
 import { IUser } from '../../../services/api/models/user-interfaces';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatListModule } from '@angular/material/list';
 import { PrivateChatService } from '../../../services/api/chat/private/private.chat.service';
 import { AuthApiService } from '../../../services/api/auth/auth-api.service';
 import { IPrivateChat } from '../../../services/api/models/private-chat-interface';
@@ -14,11 +10,10 @@ import { MessagesSharingService } from '../../../services/messages-sharing.servi
 import { NgClass } from '@angular/common';
 import { ModalService } from '../../../services/modal.service';
 
-const MATERIAL_MODULES = [MatIconModule, MatDividerModule, MatListModule];
 @Component({
   selector: 'app-chat-list',
   standalone: true,
-  imports: [MATERIAL_MODULES, NgClass, SearchDialogComponent],
+  imports: [NgClass, SearchDialogComponent],
   templateUrl: './chat-list.component.html',
   styleUrl: './chat-list.component.css',
 })
